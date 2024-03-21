b7d2b14e0a81050a1039561e016682fb8a75e4a7
Yujie Qiao <yq7946@argo-comp2.princeton.edu>
2024-03-20 20:18:03 -0400

    Updated run_parallel_main.py

--- scripts/run_parallel_main.py
+++ scripts/run_parallel_main.py
@@ -15,6 +15,16 @@ output_base_dir = "/Genomics/pritykinlab/yujie/preprocessing_benchmarking/result
 input_datasets_dir = "/Genomics/pritykinlab/yujie/preprocessing_benchmarking/datasets/harmonized_perturb_datasets"
 
 metadata_df = pd.read_csv("../dataset_metadata/selected_datasets_summary.tsv", sep='\t')
+
+# Exclude the dataset "TianKampmann2019_iPSC" from the DataFrame
+metadata_df = metadata_df[metadata_df['Dataset'] != 'TianKampmann2019_iPSC.h5ad']
+
+# metadata_df = metadata_df[metadata_df['Dataset'] != 'ReplogleWeissman2022_rpe1.h5ad']
+
+# metadata_df = metadata_df[metadata_df['Dataset'] != 'FrangiehIzar2021_RNA.h5ad']
+
+# metadata_df = metadata_df[metadata_df['Dataset'] != 'McFarlandTsherniak2020.h5ad']
+
 metadata_df = metadata_df.sort_values(['Size (GB)'])
 
 import sys
@@ -31,5 +41,5 @@ slurm_params = {
 arguments_list = [{'row':row, 'default_slurm_params':slurm_params} for idx, row in metadata_df.iterrows()]
 
 
-results = slurm_submitter.run(run_parallel.run, arguments_list, slurm_params=slurm_params, run_type='slurm')
+results = slurm_submitter.run(run_parallel.run, arguments_list, slurm_params=slurm_params, run_type='regular')
 
